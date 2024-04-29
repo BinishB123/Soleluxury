e8ae07ca230d134d353a2e0eeb23c1fcc160fb6a
BinishB123 <binishb2018@gmail.com>
2024-04-29 16:29:21 +0530

    uuuu

--- router.js/userFeat.js
+++ router.js/userFeat.js
@@ -103,6 +103,6 @@ userRoute.get('/prodcutwithsizecartcheck',userHelper.checkingUserBlockedOrNot,ca
  userRoute.post("/add-money-to-wallet",userHelper.checkingUserBlockedOrNot,walletContoller.addMoneyWallet)
  userRoute.post('/wallet-verifypayment',userHelper.checkingUserBlockedOrNot,walletContoller.verifyPayment)
 
-console.log("ok       dbfhvdb")
+console.log("ok       dbfhvdb   sreyas")
 
 module.exports = userRoute   
